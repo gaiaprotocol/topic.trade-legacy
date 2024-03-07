2076d3e486b71691aa59ed41d2c7c0ffe0597e1d
yjgaia <hanul@hanul.me>
2024-03-07 11:00:45 +0900

    Add inject_fsesf_msg

--- app/initialize.ts
+++ app/initialize.ts
@@ -1,15 +1,18 @@
-import { AppInitializer, Router } from "@common-module/app";
-import { SFEnv } from "fsesf";
+import { AppInitializer, MaterialIconSystem, Router } from "@common-module/app";
+import { SFEnv, inject_fsesf_msg } from "fsesf";
 import App from "./App.js";
 import AppConfig from "./AppConfig.js";
 
 export default async function initialize(config: AppConfig) {
+  inject_fsesf_msg();
+  MaterialIconSystem.launch();
+
   SFEnv.init({
     messageForWalletLinking: "Link Wallet to topic.trade",
     chains: config.chains,
     defaultChain: config.defaultChain,
     contractAddresses: config.contractAddresses,
-    hashtagName: "topic",
+    hashtagOptions: { name: "topic", baseUri: "" },
   });
 
   AppInitializer.initialize(
