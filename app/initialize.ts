1bb64c0d6b6511078091dd3bf5d0beaaeb1f6c6c
yjgaia <hanul@hanul.me>
2024-03-17 15:25:33 +0900

    Dev PWAInstallOverlay

--- app/initialize.ts
+++ app/initialize.ts
@@ -1,10 +1,13 @@
 import {
   AppInitializer,
   AuthUtil,
+  BodyNode,
+  BrowserInfo,
   el,
   Router,
   SplashLoader,
 } from "@common-module/app";
+import { PWAInstallOverlay } from "@common-module/social";
 import {
   inject_fsesf_msg,
   SFEnv,
@@ -13,9 +16,9 @@ import {
   WalletConnectManager,
 } from "fsesf";
 import { base, baseSepolia } from "viem/chains";
+import { FCM } from "../../social-module/lib/index.js";
 import App from "./App.js";
 import AppConfig from "./AppConfig.js";
-import { FCM } from "../../social-module/lib/index.js";
 
 export default async function initialize(config: AppConfig) {
   inject_fsesf_msg();
@@ -40,37 +43,44 @@ export default async function initialize(config: AppConfig) {
     defaultAssetPrice: 68750000000000n,
   });
 
-  AppInitializer.initialize(
-    config.supabaseUrl,
-    config.supabaseAnonKey,
-    config.dev,
-  );
+  if (BrowserInfo.isMobileDevice && !BrowserInfo.installed) {
+    new PWAInstallOverlay(SFEnv.serviceName).appendTo(BodyNode);
+  } else {
+    AppInitializer.initialize(
+      config.supabaseUrl,
+      config.supabaseAnonKey,
+      config.dev,
+    );
 
-  WalletConnectManager.init(config.walletConnectProjectId, [
-    base,
-    baseSepolia,
-  ]);
+    WalletConnectManager.init(config.walletConnectProjectId, [
+      base,
+      baseSepolia,
+    ]);
 
-  FCM.init(
-    {
-      apiKey: "AIzaSyBZCRpj9smnz-yIpXC4KVi9RFs23qcxH7M",
-      authDomain: "topictrade-8b711.firebaseapp.com",
-      projectId: "topictrade-8b711",
-      storageBucket: "topictrade-8b711.appspot.com",
-      messagingSenderId: "993631591207",
-      appId: "1:993631591207:web:d7bec5f0e54efdfe2ee702",
-      measurementId: "G-9CNQ54G1CY",
-    },
-    "BKhZmi9lpQlQhFXwyMNujFGfjXQEfWKNML8S2gzu6hcFGr1pL-vPOTPU5YwtFHJ4poW-Ax7qm9xeR-7AB76eGl4",
-  );
+    FCM.init(
+      {
+        apiKey: "AIzaSyBZCRpj9smnz-yIpXC4KVi9RFs23qcxH7M",
+        authDomain: "topictrade-8b711.firebaseapp.com",
+        projectId: "topictrade-8b711",
+        storageBucket: "topictrade-8b711.appspot.com",
+        messagingSenderId: "993631591207",
+        appId: "1:993631591207:web:d7bec5f0e54efdfe2ee702",
+        measurementId: "G-9CNQ54G1CY",
+      },
+      "BKhZmi9lpQlQhFXwyMNujFGfjXQEfWKNML8S2gzu6hcFGr1pL-vPOTPU5YwtFHJ4poW-Ax7qm9xeR-7AB76eGl4",
+    );
 
-  await SplashLoader.load(el("img", { src: "/images/logo-transparent.png" }), [
-    SFSignedUserManager.fetchUserOnInit(),
-  ]);
+    await SplashLoader.load(
+      el("img", { src: "/images/logo-transparent.png" }),
+      [
+        SFSignedUserManager.fetchUserOnInit(),
+      ],
+    );
 
-  SFOnlineUserManager.init();
+    SFOnlineUserManager.init();
 
-  Router.route(["", "{topic}"], App);
+    Router.route(["", "{topic}"], App);
 
-  AuthUtil.checkEmailAccess();
+    AuthUtil.checkEmailAccess();
+  }
 }
